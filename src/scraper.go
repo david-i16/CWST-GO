cfa7249ef3e2070b9075c342cd2587fee8868f0b
david-i16 <71136429+david-i16@users.noreply.github.com>
2022-08-15 17:29:57 +0300

    colly collector for scraper

--- src/scraper.go
+++ src/scraper.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/csv"
 	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/gocolly/colly"
 )
 
-func destFile(x string) (f os.File) {
+/*func destFile(x string) (f os.File){
 	fName := filepath.Join("D:\\", "go projects", "cwst go", "CWST-GO", "target folder", x)
 	file, err := os.Create(fName)
 	if err != nil {
@@ -14,13 +17,24 @@ func destFile(x string) (f os.File) {
 	}
 	defer file.Close()
 	return *file
-}
+}*/
 
 func main() {
 	//setting up the file where we store collected data
-	//destFile("index.csv")
+	fName := filepath.Join("D:\\", "go projects", "cwst go", "CWST-GO", "target folder", "index.csv")
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Could not create file, error :%q", err)
+	}
+	defer file.Close()
 	//writer that writes the collected data into our file
-	//writer := csv.NewWriter(file)
+	writer := csv.NewWriter(file)
 	//after the file is written, what it is in the buffer goes in writer and then passed to file
-	//defer writer.Flush()
+	defer writer.Flush()
+
+	//collector
+	c := colly.NewCollector(
+		colly.AllowedDomains("olx.ro"),
+	)
+
 }
