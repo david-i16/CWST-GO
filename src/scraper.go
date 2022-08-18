d5ce0c80abfbd27410fdb377df63a552b9d78a07
david-i16 <71136429+david-i16@users.noreply.github.com>
2022-08-18 17:02:45 +0300

    multiple pages visited + Concurrency try (fail)

--- src/scraper.go
+++ src/scraper.go
@@ -28,27 +28,46 @@ func main() {
 		log.Fatalf("Could not create file, error :%q", err)
 	}
 	defer file.Close()
+
 	//writer that writes the collected data into our file
 	writer := csv.NewWriter(file)
+
+	//channel for output(seller name)
+	//y := make(chan string)
+
 	//after the file is written, what it is in the buffer goes in writer and then passed to file
 	defer writer.Flush()
 
 	//collector
 	c := colly.NewCollector(
-		colly.AllowedDomains("https://www.olx.ro/"),
+		colly.AllowedDomains("www.olx.ro"),
 	)
 
 	//HTML parser
 	c.OnHTML(".css-1fp4ipz", func(e *colly.HTMLElement) { //div class that contains wanted info
 
+		//y <- e.ChildText("h4")
+
+		//z := <-y
+
 		writer.Write([]string{
 			e.ChildText("h4"), //specific tag of the info
+
+			//z,  instead of previous line(not working though)
+
 			//e.ChildText("span"),
 		})
 	})
 
-	fmt.Printf("Scraping page :  ")
+	//visiting 4 target pages
+	fmt.Printf("Scraping page 1 ... \n")
 	c.Visit("https://www.olx.ro/d/oferta/bmw-xdrixe-seria-7-2020-71000-tva-IDgp7iN.html")
+	fmt.Printf("Scraping page 2 ... \n")
+	c.Visit("https://www.olx.ro/d/oferta/televizor-smart-qled-samsung-75qn900a-189-cm-ultra-hd-8k-neo-qled-IDfR6sI.html")
+	fmt.Printf("Scraping page 3 ... \n")
+	c.Visit("https://www.olx.ro/d/oferta/vand-casa-in-gai-IDgpIH8.html")
+	fmt.Printf("Scraping page 4 ... \n")
+	c.Visit("https://www.olx.ro/d/oferta/suport-tac-biliard-din-2-bucati-IDgo4s9.html")
 
 	log.Printf("\n\nScraping Complete\n\n")
 	log.Println(c)
